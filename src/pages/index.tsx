6791732f3af0a824f581db4c790eb11737b4cd69
wato787 <wato787@icloud.com>
2023-03-22 00:44:46 +0900

    firebase連携前

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,7 +26,7 @@ export default function Login() {
       >
         <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
           <Stack align={"center"}>
-            <Heading fontSize={"4xl"}>Sign in to your account</Heading>
+            <Heading fontSize={"4xl"}>サインイン</Heading>
           </Stack>
           <Box
             rounded={"lg"}
