0d958cd405094f31e0c13c1c03016e5da5be57c7
wato787 <wato787@icloud.com>
2023-04-08 12:30:23 +0900

    タイトル変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,25 @@
 import "@/styles/globals.css";
 
-import { ChakraProvider} from '@chakra-ui/react';
+import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 import { RecoilRoot } from "recoil";
-import { theme } from '../styles/theme';
+import { theme } from "../styles/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
-
-  
   return (
     <>
       <ChakraProvider theme={theme}>
         <RecoilRoot>
-          
+          <Head>
+            <title>Ohung</title>
+            <link
+              rel="apple-touch-icon"
+              sizes="180x180"
+              href="../../public/icon.jpeg"
+            />
+          </Head>
           <Component {...pageProps} />
         </RecoilRoot>
       </ChakraProvider>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,10 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="ja">
+
       <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-      <link rel="apple-touch-icon" sizes="180x180" href="../../public/icon.jpeg" />
 
         <link
           href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital@1&display=swap"
