32bf6586c8768b4c7edeee6ccc746c2631cafb23
wato787 <wato787@icloud.com>
2023-04-08 11:32:03 +0900

    いらない機能削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,25 +2,12 @@ import "@/styles/globals.css";
 
 import { ChakraProvider} from '@chakra-ui/react';
 import type { AppProps } from "next/app";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+
 import { RecoilRoot } from "recoil";
 import { theme } from '../styles/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
 
-  useEffect(() => {
-    const currentUrl = router.asPath;
-    const existingTab = Array.from(window.parent.frames)
-      .find(frame => frame.location.href === currentUrl);
-
-    if (existingTab) {
-      existingTab.focus();
-    } else {
-      window.open(currentUrl, '_blank');
-    }
-  }, [router]);
   
   return (
     <>
